cdf382323cd1e23e546e591a6b612a233ad7541e
Travis <travis@mooremediallc.com>
2023-01-23 06:53:11 -0700

    fix operation double button press issue

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ bottom_display.innerHTML = '0';
 let operation_count = 0;
 
 let current_number = '';
+let current_log = '';
+let current_sign = '';
 
 const calc_obj = {
     total: 0,
@@ -55,6 +57,7 @@ clear_btn.forEach((btn) => {
         operation_count = 0;
 
         current_number = '';
+        current_log = '';
 
         calc_obj.total = 0;
         calc_obj.next = 0;
@@ -101,14 +104,21 @@ plus_minus_btn.forEach((btn) => {
 number_btn.forEach((btn) => {
     btn.addEventListener('click', () => {
 
+        //Clears bottom display value
+        bottom_display.innerHTML = '';
+
+        //Take current number var and basicly turns it into array
         current_number = current_number + btn.innerHTML;
-        //console.log(current_number);
         
+        //Logs values in arr
+        current_log = current_log + btn.innerHTML;
+
+        //Top display
+        top_display.innerHTML = current_log
+        
+        //Bottom display
         bottom_display.innerHTML = current_number;
-        console.log("current Number: " + current_number);
-        console.log("Obj Total: " + calc_obj.total);
-        console.log("Obj Next: " + calc_obj.next);
-        console.log("Obj Sign: " + calc_obj.sign);
+    
 
     });
 });
@@ -116,14 +126,27 @@ number_btn.forEach((btn) => {
 delete_btn.forEach((btn) => {
     btn.addEventListener('click', () => {
 
-        if(current_number.length <= 1)
+        if(current_number.length < 1)
         {
             current_number = '';
+            current_log = '';
+            top_display.innerHTML = '';
             bottom_display.innerHTML = '0';
+
+            current_number = '';
+            current_log = '';
+            current_sign = '';
+
+
         }
         else 
         {
+            current_log = current_log.slice(0,-1)
             current_number = current_number.slice(0,-1);
+            console.log(current_number);
+           
+            //Display
+            top_display.innerHTML = current_log;
             bottom_display.innerHTML = current_number;
         }
 
@@ -135,44 +158,77 @@ delete_btn.forEach((btn) => {
 operation_btn.forEach((btn) => {
     btn.addEventListener('click', () => {
 
-
-        if(operation_count >= 1)
+        if(calc_obj.sign != current_sign)
         {
-            
-            calc_obj.next = parseFloat(current_number);
-            
 
-            calc_obj.total = operate(calc_obj.total, calc_obj.next, calc_obj.sign);
+            if(operation_count > 0)
+            {
+                
+                //Clear display
+                bottom_display.innerHTML = '';
 
-            console.log("if Totallll: " + calc_obj.total);
-            //calc_obj.total = calc_obj.total + calc_obj.next;
-            bottom_display.innerHTML = calc_obj.total;
-            
-            
-            //Clears current number
-            current_number = ''
+                //Add current number into object next key
+                calc_obj.next = parseFloat(current_number);
+                
+                //Calculate 
+                calc_obj.total = operate(calc_obj.total, calc_obj.next, calc_obj.sign);
 
-            //changes operation sign in object
-            calc_obj.sign = btn.innerHTML;
-    
+                //Display calculation on main display
+                bottom_display.innerHTML = calc_obj.total;
+
+                //Add current sign to temp sign var
+                current_sign = btn.innerHTML;
+
+                //Add sign to top log display
+                top_display.innerHTML = current_log + btn.innerHTML;
+
+                //Add new operator sign to object
+                calc_obj.sign = current_sign;
+
+                //Display log in top display
+                current_log = current_log + btn.innerHTML;
+                top_display.innerHTML = current_log;
+                
+                //clear temp var
+                current_number = '';
+                
+
+        
+            }
+            else {
+
+
+
+                //Add current number into object next key
+                calc_obj.next = parseFloat(current_number);
+
+                //Add next into object total key
+                calc_obj.total = calc_obj.next;
+
+
+                //Display log in top display
+                current_log = current_log + btn.innerHTML;
+                top_display.innerHTML = current_log;
+
+                //Clear display then display value in bottom display
+                bottom_display.innerHTML = '';
+                current_sign = btn.innerHTML;
+
+                //Add calc sign into object
+                calc_obj.sign = current_sign;
+
+                //display sign to bottom variable
+                bottom_display.innerHTML = current_sign;
+
+                //Clear current number temp var
+                current_number = '';
+
+
+            }
+            operation_count++;
         }
-        else {
 
-            calc_obj.sign = btn.innerHTML;
-            
-            calc_obj.next = parseFloat(current_number);
-            console.log("else Next: " + calc_obj.next);
 
-            calc_obj.total = calc_obj.total + calc_obj.next;
-            console.log("else Total: " + calc_obj.total);
-
-            bottom_display.innerHTML = current_number;
-
-            current_number = ''
-
-        }
-
-        operation_count++;
 
 
     });
@@ -181,16 +237,7 @@ operation_btn.forEach((btn) => {
 equal_btn.forEach((btn) => {
     btn.addEventListener('click', () => {
         
-        calc_obj.next = parseFloat(current_number);
-
-        calc_obj.total =  operate(calc_obj.total, calc_obj.next, calc_obj.sign);
-
-        calc_obj.next = calc_obj.total;
-
-        //Update display 
-        bottom_display.innerHTML = calc_obj.total;
-
-        current_number = '';
+      
 
     });
 });
