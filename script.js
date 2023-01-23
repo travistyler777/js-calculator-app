72e678ae63fa1876fd800f9f5789eb10eff199d7
Travis <travis@mooremediallc.com>
2023-01-22 18:49:00 -0700

    Fix equal > operation calculations

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ let top_display = document.querySelector('.top-display');
 let bottom_display = document.querySelector('.bottom-display');
 
 top_display.innerHTML = '';
-bottom_display.innerHTML = '';
+bottom_display.innerHTML = '0';
 
 let operation_count = 0;
 
 let current_number = '';
 
 const calc_obj = {
-    total: '',
-    next: '',
+    total: 0,
+    next: 0,
     sign: ''
 };
 
@@ -50,16 +50,51 @@ function operate(total,next,sign)
 clear_btn.forEach((btn) => {
     btn.addEventListener('click', () => {
         top_display.innerHTML = '';
-        bottom_display.innerHTML = '';
+        bottom_display.innerHTML = '0';
 
         operation_count = 0;
 
         current_number = '';
 
-        calc_obj.total = '';
-        calc_obj.next = '';
+        calc_obj.total = 0;
+        calc_obj.next = 0;
         calc_obj.sign = '';
 
+    });
+});
+
+plus_minus_btn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+
+        
+        if (current_number < 0)
+        {
+            //Convert string to number
+            current_number = parseFloat(current_number);
+    
+            //Turn negative into positive
+            current_number = Math.abs(current_number)
+    
+            //Convert back to string
+            current_number = current_number.toString()
+
+            //Display
+            bottom_display.innerHTML = current_number;
+        }
+        else {
+            //Convert string to number
+            current_number = parseFloat(current_number);
+
+            //Turn negative into negative
+            current_number = -Math.abs(current_number)
+
+            //Convert back to string
+            current_number = current_number.toString()
+
+            //Display
+            bottom_display.innerHTML = current_number;
+        }
+  
     });
 });
 
@@ -67,9 +102,13 @@ number_btn.forEach((btn) => {
     btn.addEventListener('click', () => {
 
         current_number = current_number + btn.innerHTML;
-        console.log(current_number);
+        //console.log(current_number);
         
         bottom_display.innerHTML = current_number;
+        console.log("current Number: " + current_number);
+        console.log("Obj Total: " + calc_obj.total);
+        console.log("Obj Next: " + calc_obj.next);
+        console.log("Obj Sign: " + calc_obj.sign);
 
     });
 });
@@ -77,8 +116,16 @@ number_btn.forEach((btn) => {
 delete_btn.forEach((btn) => {
     btn.addEventListener('click', () => {
 
-        current_number = current_number.slice(0,-1);
-        bottom_display.innerHTML = current_number;
+        if(current_number.length <= 1)
+        {
+            current_number = '';
+            bottom_display.innerHTML = '0';
+        }
+        else 
+        {
+            current_number = current_number.slice(0,-1);
+            bottom_display.innerHTML = current_number;
+        }
 
     });
 });
@@ -88,26 +135,62 @@ delete_btn.forEach((btn) => {
 operation_btn.forEach((btn) => {
     btn.addEventListener('click', () => {
 
+
         if(operation_count >= 1)
         {
-            //Add number total into calc next
-            //calc_obj.next = parceFloat(current_number)
+            
+            calc_obj.next = parseFloat(current_number);
+            
+
+            calc_obj.total = operate(calc_obj.total, calc_obj.next, calc_obj.sign);
+
+            console.log("if Totallll: " + calc_obj.total);
+            //calc_obj.total = calc_obj.total + calc_obj.next;
+            bottom_display.innerHTML = calc_obj.total;
+            
+            
+            //Clears current number
+            current_number = ''
+
+            //changes operation sign in object
+            calc_obj.sign = btn.innerHTML;
+    
         }
         else {
-            //add number total into calc total
-            //calc_obj.total = parceFloat(current_number)
+
+            calc_obj.sign = btn.innerHTML;
+            
+            calc_obj.next = parseFloat(current_number);
+            console.log("else Next: " + calc_obj.next);
+
+            calc_obj.total = calc_obj.total + calc_obj.next;
+            console.log("else Total: " + calc_obj.total);
+
+            bottom_display.innerHTML = current_number;
+
+            current_number = ''
+
         }
 
         operation_count++;
+
 
     });
 });
 
 equal_btn.forEach((btn) => {
     btn.addEventListener('click', () => {
+        
+        calc_obj.next = parseFloat(current_number);
 
-        //total = total + next number
+        calc_obj.total =  operate(calc_obj.total, calc_obj.next, calc_obj.sign);
+
+        calc_obj.next = calc_obj.total;
+
         //Update display 
+        bottom_display.innerHTML = calc_obj.total;
+
+        current_number = '';
 
     });
 });
